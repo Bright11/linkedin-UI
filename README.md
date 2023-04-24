ddc3697ebdfc4f6848e507d6e22a556e68618da0
Bright11 <chikanwazuo@gmail.com>
2023-04-24 17:37:41 +0000

    linkedin UI

---
+++ README.md
@@ -0,0 +1,8 @@
+## Linkedin UI design
+-- a template build using html, css, and javascript with dropdown just like linkedin
+## pages
+-- Only the frontend page
+## Terms of use
+-- just clone it and opened it on your web browser
+## Respnsiveness
+-- very responsive
---
+++ js.js
@@ -0,0 +1,19 @@
+
+var showdropdwon = document.querySelector(".bs-dropdwon");
+function showbusiness() {
+	showdropdwon.classList.toggle("show")
+}
+
+
+var topfeed = document.querySelector(".toppostdropdown");
+
+function sortdrop() {
+	topfeed.classList.toggle("opentopfeed")
+}
+
+// savedropdown
+savedropdown = document.querySelector(".savedropdown");
+
+function opendropdown() {
+	savedropdown.classList.toggle("seedropdown");
+}
